3ccc7b9432da5518019471f316c2ebee661a7cea
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2023-05-12 19:04:00 +0300

    Update avatar.js

--- routes/avatar.js
+++ routes/avatar.js
@@ -1,37 +1,69 @@
+import dotenv from "dotenv";
+dotenv.config();
+import AWS from "aws-sdk"
+import multerS3 from "multer-s3"
 import { Router } from "express";
 import multer from "multer";
-// import { User } from "../model/users.js";
 import authenticate from "../middlewere/auth.js";
 import fs from "fs";
-const router =Router()
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "images");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() +"-"+ file.originalname);
-  },
+import express from "express"
+const app=express()
+const router = Router();
+
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  region: process.env.REGION,
 });
-const upload = multer({ storage: storage });
+const BUCKET = process.env.BUCKET;
+const s3 = new AWS.S3();
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ['image/jpeg', 'image/png'];
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error('Invalid file type. Only JPEG and PNG files are allowed.');
+    error.code = 'LIMIT_FILE_TYPES';
+    return cb(error, false);
+  }
+  cb(null, true);
+};
+
+const limits = {
+  fileSize: 5 * 1024 * 1024, // 4 megabytes
+};
+
+const upload = multer({
+  fileFilter: fileFilter,
+    limits: limits,
+  storage: multerS3({
+    s3: s3,
+    bucket: BUCKET,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString() + "-" + file.originalname);
+    },
+  }),
+});
+
 router.post("/", authenticate, upload.single("file"), async (req, res) => {
-    if (req.user.img === "/images/avatar.png") {
-      req.user.img = "/Images/"+req.file.filename;
-      await req.user.save();
-      console.log(true)
-    } else {
-      console.log("req.user.img");
-      console.log(req.user.img);
-        fs.unlink(`./${req.user.img}`,(err) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          console.log(`File`);
-          console.log(req.file);
-        req.user.img = "/Images/" + req.file.filename;
-          req.user.save();
-        }); 
-    }
+  console.log(req.file)
+  console.log(req.file)
+  if (req.user.img === "/images/avatar.png") {
+    req.user.img = req.file.location;
+    await req.user.save();
+  } else {
+    let x = await s3
+      .deleteObject({ Bucket: BUCKET, Key: req.user.img })
+      .promise();
+    console.log(`File`);
+    console.log(req.file);
+    req.user.img = req.file.location;
+    req.user.save();
+  }
 });
+
 
 export default router;
